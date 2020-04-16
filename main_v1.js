58e07773881ca48ea0158c43e1c4eb1bb829c5ab
ThaDemonReaper <60007540+ThaDemonReaper@users.noreply.github.com>
2020-04-16 04:12:45 +0300

    Add files via upload

---
+++ main_v1.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("Left-nav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+function closeNav() {
+    document.getElementById("Left-nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+function openNav2() {
+    document.getElementById("Left-nav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+function closeNav2() {
+    document.getElementById("Left-nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
